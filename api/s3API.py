65fa4984e36720741bbf49fa3b14b37a82567eb2
heoseongh <shheo0807@gmail.com>
2021-05-28 11:20:40 +0900

    Add: S3 다운로드 API 추가

--- api/s3API.py
+++ api/s3API.py
@@ -1,5 +1,6 @@
 import sys
 import os
+
 # 해당 코드는 s3API를 루트 디렉터리로 올려놓는 것과 같은 효과를 준다. -> 그래야 모듈 임포트시 에러가 뜨지 않는다.
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
@@ -10,10 +11,11 @@ from aws.config import (
     BUCKET_NAME
 )
 
-# @description: S3 연결 메서드
-def s3Connection():
+# @description: S3 클라이언트 연결 메서드
+# @return s3 (Object)
+def s3ConnectionClient():
     
-    # s3 연결
+    # s3 클라이언트 연결
     s3 = boto3.client(
         's3', 
         aws_access_key_id = AWS_ACCESS_KEY, 
@@ -21,15 +23,46 @@ def s3Connection():
     )
     return s3
 
-# @description: S3 파일 업로드 메서드
-# @param: file_path, file_name (string, string)
-def uploadFile(file_path, file_name):
+# @description: S3 리소스 연결 메서드
+# @return s3 (Object)
+def s3ConnectionResource():
     
-    s3 = s3Connection() # s3 연결
+    # s3 리소스 연결
+    s3 = boto3.resource(
+        's3', 
+        aws_access_key_id = AWS_ACCESS_KEY, 
+        aws_secret_access_key = AWS_SECRET_KEY
+    )
+    return s3
+        
+
+# @description: S3 파일 업로드 메서드
+# @param: local_file, bucket_file (string, string)
+def uploadFile(local_file, bucket_file):
+    
+    s3 = s3ConnectionClient() # s3 클라이언트 연결
 
     # 파일 업로드
     s3.put_object(
         Bucket = BUCKET_NAME, # 버킷 이름
-        Body = file_path, # 업로드할 파일 경로
-        Key = file_name, # 업로드할 파일명
+        Body = local_file, # 업로드할 파일 경로
+        Key = bucket_file, # 업로드할 파일명
     )
+
+# @description: S3 파일 다운로드 메서드
+# @param: local_file, bucket_file (string, string)
+def downloadFile(local_file, bucket_file):
+    import botocore
+
+    s3 = s3ConnectionResource() # S3 리소스 연결
+
+    try:
+        # 파일 다운로드
+        s3.Bucket(BUCKET_NAME).download_file(bucket_file, local_file)
+        print('파일이 성공적으로 저장되었습니다.')
+        
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            print('해당 파일이 S3에 없습니다.')
+        else:
+            raise
