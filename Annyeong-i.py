74e90773a5f8e96896e0214f89e4310318b426b0
heoseongh <shheo0807@gmail.com>
2021-05-31 21:24:09 +0900

    Refactor: musicplayer 패키지 사용하여 음악 재생 코드 리팩토링

--- Annyeong-i.py
+++ Annyeong-i.py
@@ -1,6 +1,7 @@
-from __future__ import division
-from json import load
+# from __future__ import division
+# from json import load
 
+from modules.musicplayer import MusicPlayer
 import re
 import sys
 
@@ -60,7 +61,7 @@ class MicrophoneStream(object):
         self._audio_interface.terminate()
 
     def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        """Continuously collect data from the audio stream, into the buffer."""
+        """버퍼에서 계속 오디오 스트림 데이터를 수집한다."""
         self._buff.put(in_data)
         return None, pyaudio.paContinue
 
@@ -120,50 +121,78 @@ def listen_print_loop(responses):
             num_chars_printed = len(transcript)
 
         else:
-            # 안녕이 시작하기
-            # 다음의 단어로 시작 가능
-            # start keyword: 시작, 좋은 아침, 안녕, 나왔어
-            # 한번만 작동시키기 위해서 (isStarted == False) 조건 추가
-            if re.search(r"\b(시작|좋은 아침|안녕|나왔어)\b", transcript, re.I) and isStarted == False:
-                isStarted = True
-                wlecome = '안녕이 서비스를 시작합니다.'
-                print(wlecome)
-                API.toSpeech(wlecome)
-                print('==================================')
-                continue
-
-            # 안녕이 서비스 동작 중인 경우
-            if isStarted:
-                # 안녕이 종료하기
-                # 다음의 단어로 종료 가능
-                # stop keyword: 종료, 잘자, 잘게, 잔다, 바이바이
-                if re.search(r"\b(종료|잘자|잘게|잔다|바이바이)\b", transcript, re.I):
-                    print("안녕이 서비스를 종료합니다.")
-                    # 대화 저장하기
-                    with open('conversation.json', 'w') as f:
-                        f.write(json.dumps(conversations, ensure_ascii=False))
-
-                    print("총 " + str(len(conversations)) + "건의 대화가 저장되었습니다.")
-                    isStarted = False
+            # 안녕이 서비스가 동작하지 않고 있는 경우(isStarted == False) 다음의 방법으로 안녕이를 시작할 수 있다.
+            if not isStarted :
+                # 다음의 단어들 중 한 단어 이상 포함시켜서 말하면 안녕이 서비스가 시작된다.
+                # start keyword: 시작, 좋은 아침, 안녕, 나왔어
+                if re.search(r"\b(시작|좋은 아침|안녕|나왔어)\b", transcript, re.I):
+                    isStarted = True # 서비스 시작 플래그 셋팅
+                    # 안녕이 서비스 시작 멘트
+                    print("안녕이 서비스를 시작합니다.")
+                    API.toSpeech("안녕이 서비스를 시작합니다.")
+                    print('==================================')
+                    # 안녕이가 먼저 첫 질문을 건네며 대화를 시작한다.
+                    API.toSpeech("오늘 하루 어떠셨나요?") # TODO 스타트를 어떻게 끊을지 생각해봐야 할 듯
+                    print("오늘 하루 어떠셨나요?")
+                    import time
+                    time.sleep(1)
                     continue
 
+            # 안녕이 서비스 동작 중인 경우(isStarted == True)
+            if isStarted:
+                # 안녕이 종료하기 
+                # 지정해놓은 단어를 말하면 종료할 수 있다.
+                # stop keyword: 종료, 잘자, 잘게, 잔다, 바이바이
+                if re.search(r"\b(종료|잘자|잘게|잔다|바이바이)\b", transcript, re.I):
+                    API.toSpeech("안녕이 서비스를 종료합니다.")
+                    print("안녕이 서비스를 종료합니다.")
+                    # 대화 내용 파일에 저장하기
+                    with open('conversation.json', 'w') as f:
+                        f.write(json.dumps(conversations, ensure_ascii=False))
+                    print("총 " + str(len(conversations)) + "건의 대화가 저장되었습니다.")
+                    isStarted = False # 서비스 종료 플래그 셋팅
+                    continue
+
+                # if re.search(r"\b(다음|다음 노래)\b", transcript, re.I):
+                #     print("ㅎㅎ")
                 else:
                     print('사용자: ' + transcript + overwrite_chars)
-                    
-                    # 안녕이에게 답장 받아오기
-                    answer = API.getAnswer(transcript)
-                    res_data = API.getResponseData(transcript)
+                    # 안녕이 답장 받아오기 (답장에는 감정 데이터도 함께 온다.)
+                    (answer, emotion) = API.getResponseData(transcript)
                     print('안녕이: ' + answer)
-                    API.toSpeech(answer)
-                    print('감정: ' + API.getEmotion(res_data))
+                    print('감정분석: ' + emotion)
                     print('----------------')
-                    
-                    # 대화 저장
-                    UUID = str(uuid.uuid1()) # 객체 구별을 위한 랜덤 값 생성
-                    conversation = { "id": UUID,"user": transcript, "answer": answer, "emotion": API.getEmotion(res_data)}
-                    conversations.append(conversation)
+                    API.toSpeech(answer) # 답장 음성으로 송출
 
-            
+                    # 만약 감정이 '우울'이라면, 우울한 감정에 맞는 노래를 송출해준다.
+                    if(emotion == '우울'):
+                        API.toSpeech("제가 음악 한 곡 들려드릴게요. 이 음악 듣고 위로가 되었으면 좋겠어요.")
+                        # TODO S3에서 음악파일 다운로드 API 호출
+                        # Nagative Music List 목록 받기
+                        musicFiles = [
+                            "../Music/Nagative/n-1-001.mp3","../Music/Nagative/n-2-001.mp3","../Music/Nagative/n-3-001.mp3"]
+                        # 뮤직플레이어 생성 (음악 파일 리스트, 플레이 타입 설정)
+                        musicPlayer = MusicPlayer(musicFiles, playType='random')
+                        # 음악 재생
+                        musicPlayer.playMusic()
+                        continue
+
+            # # 플레이리스트 탐색 기능
+            # track = 0 # 음악 트랙
+            # if re.search(r"\b(다음 노래|다른 음악|)\b", transcript, re.I):
+            #     # 플레이 리스트에 없는 트랙이라면
+            #     if(not track in randomPlayList):
+            #         track = 0 # 트랙 초기화
+            #     # 아니면
+            #     else: 
+            #         track += 1 # 다음 트랙 탐색
+            #     # API.musicPlay(playList, randomPlayList, track)
+                
+                # 대화 배열에 적재하기
+                UUID = str(uuid.uuid1()) # 객체 구별을 위한 랜덤 값 생성
+                conversation = { "id": UUID, "user": transcript, "answer": answer, "emotion": emotion}
+                conversations.append(conversation)
+
             num_chars_printed = 0
 
 def main():
@@ -189,10 +218,11 @@ def main():
     with MicrophoneStream(RATE, CHUNK) as stream:
         audio_generator = stream.generator()
         requests = (
-            speech.StreamingRecognizeRequest(audio_content=content)
+            speech.StreamingRecognizeRequest
+            (audio_content=content)
             for content in audio_generator
         )
-
+        
         responses = client.streaming_recognize(streaming_config, requests)
 
         # 실제로 화면에 뿌려주는 부분
