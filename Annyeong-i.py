f3ccee2c851fd7c4b00849263360c6e9811ee938
heoseongh <shheo0807@gmail.com>
2021-06-01 11:01:35 +0900

    Refactor: 전반적인 코드 수정

--- Annyeong-i.py
+++ Annyeong-i.py
@@ -1,6 +1,3 @@
-# from __future__ import division
-# from json import load
-
 from packages.musicplayer import MusicPlayer
 import re
 import sys
@@ -14,9 +11,10 @@ import uuid
 import json
 import api.Index as API # 안녕이 서버 모듈 임포트
 
-# 실시간 스트림 서비스는 최대 5분 까지만 제공이 가능하다.
+# 스트림은 연속으로 305초까지만 열어놓을 수 있다.
+# TODO 305초 이후에 꺼진 스트림을 다시 열어놓을 방법 생각하기.
 
-# Audio recording parameters
+# 음성 녹음시에 필요한 파라미터
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
 
@@ -89,11 +87,29 @@ class MicrophoneStream(object):
 
 def listen_print_loop(responses):
     
-    isStarted = False
+    serviceStarted = False
+    musicStarted = False
     conversations = []
 
     num_chars_printed = 0
     for response in responses:
+        """
+        response 클래스에는 음성에서 텍스트로 변환되어 온 단어 하나하나가 transcript 필드에 담겨있다.
+        response 형식은 json과 비슷하다.
+        {
+            alternatives 
+            {
+                transcript: "Hello"
+            }
+            stability: 0.009999999776482582
+            result_end_time 
+            {
+                seconds: 2
+                nanos: 250000000
+            }
+        }
+        타입 = <class 'google.cloud.speech_v1.types.cloud_speech.StreamingRecognizeResponse'>
+        """
         if not response.results:
             continue
 
@@ -101,32 +117,39 @@ def listen_print_loop(responses):
         # the first result being considered, since once it's `is_final`, it
         # moves on to considering the next utterance.
         result = response.results[0]
+        
         if not result.alternatives:
             continue
-
-        # 최종적으로 변환된 스크립트
+        
+        # 실제 변환된 텍스트가 담긴 부분
         transcript = result.alternatives[0].transcript
 
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
+        # 이전에 출력된 텍스트를 덮어씌우기 위한 공백
         overwrite_chars = " " * (num_chars_printed - len(transcript))
-        
+
         if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + "\r")
+            """
+            다음 결과와 같이 이전에 출력된 텍스트를 공백으로 덮어씌우면서 출력하여 마치 하나의 문장인 것 처럼 보여준다.
+            >>> 안녕
+            >>> 안녕하
+            >>> 안녕하세요
+            >>> result: 안녕하세요 
+            문장의 마지막 단어가 아니라면
+            이전에 출력된 텍스트 길이(num_chars_printed)에서 현재 발화의 길이(len(transcript))만큼을 공백으로 덮어씌우고,
+            커서를 맨 앞으로 캐리지리턴(carriage return) 한다.
+            """
+            sys.stdout.write(transcript + overwrite_chars + "\r") 
             sys.stdout.flush()
 
             num_chars_printed = len(transcript)
 
         else:
-            # 안녕이 서비스가 동작하지 않고 있는 경우(isStarted == False) 다음의 방법으로 안녕이를 시작할 수 있다.
-            if not isStarted :
+            # 안녕이 서비스가 동작하지 않고 있는 경우(serviceStarted == False) 다음의 방법으로 안녕이를 시작할 수 있다.
+            if not serviceStarted:
                 # 다음의 단어들 중 한 단어 이상 포함시켜서 말하면 안녕이 서비스가 시작된다.
                 # start keyword: 시작, 좋은 아침, 안녕, 나왔어
                 if re.search(r"\b(시작|좋은 아침|안녕|나왔어)\b", transcript, re.I):
-                    isStarted = True # 서비스 시작 플래그 셋팅
+                    serviceStarted = True # 서비스 시작 플래그 셋팅
                     # 안녕이 서비스 시작 멘트
                     print("안녕이 서비스를 시작합니다.")
                     API.toSpeech("안녕이 서비스를 시작합니다.")
@@ -134,28 +157,45 @@ def listen_print_loop(responses):
                     # 안녕이가 먼저 첫 질문을 건네며 대화를 시작한다.
                     API.toSpeech("오늘 하루 어떠셨나요?") # TODO 스타트를 어떻게 끊을지 생각해봐야 할 듯
                     print("오늘 하루 어떠셨나요?")
-                    import time
-                    time.sleep(1)
                     continue
+            
+            if serviceStarted:
+                if musicStarted:
+                    """
+                    음악이 재생중인 경우, 음성으로 컨트롤할 수 있는 기능들이다.
+                    """
+                    # 음악 종료 기능
+                    if re.search(r"\b(음악 종료|음악 꺼줘|그만 들을래)\b", transcript, re.I):
+                        API.toSpeech("음악 재생을 종료합니다.")
+                        musicPlayer.stopMusic()
+                        musicStarted = False
+                        continue
+                    # 다음 음악 재생 기능
+                    if re.search(r"\b(다음 노래|다른 음악)\b", transcript, re.I):
+                        musicPlayer.stopMusic()
+                        API.toSpeech("다음 음악을 재생합니다.")
+                        musicPlayer.playNextMusic()
+                        continue
 
-            # 안녕이 서비스 동작 중인 경우(isStarted == True)
-            if isStarted:
                 # 안녕이 종료하기 
                 # 지정해놓은 단어를 말하면 종료할 수 있다.
                 # stop keyword: 종료, 잘자, 잘게, 잔다, 바이바이
-                if re.search(r"\b(종료|잘자|잘게|잔다|바이바이)\b", transcript, re.I):
+                elif re.search(r"\b(종료|잘자|잘게|잔다|바이바이)\b", transcript, re.I):
                     API.toSpeech("안녕이 서비스를 종료합니다.")
                     print("안녕이 서비스를 종료합니다.")
                     # 대화 내용 파일에 저장하기
                     with open('conversation.json', 'w') as f:
                         f.write(json.dumps(conversations, ensure_ascii=False))
                     print("총 " + str(len(conversations)) + "건의 대화가 저장되었습니다.")
-                    isStarted = False # 서비스 종료 플래그 셋팅
+                    serviceStarted = False # 서비스 종료 플래그 셋팅
                     continue
 
-                # if re.search(r"\b(다음|다음 노래)\b", transcript, re.I):
-                #     print("ㅎㅎ")
                 else:
+                    """
+                    사용자가 안녕이와 대화 가능한 조건은
+                    1. 안녕이 서비스가 동작중이고,
+                    2. 음악이 재생되고 있지 않는 경우이다.
+                    """
                     print('사용자: ' + transcript + overwrite_chars)
                     # 안녕이 답장 받아오기 (답장에는 감정 데이터도 함께 온다.)
                     (answer, emotion) = API.getResponseData(transcript)
@@ -175,18 +215,9 @@ def listen_print_loop(responses):
                         musicPlayer = MusicPlayer(musicFiles, playType='random')
                         # 음악 재생
                         musicPlayer.playMusic()
+                        # 음악이 재생되었다는 플래그 설정
+                        musicStarted = True
                         continue
-
-            # # 플레이리스트 탐색 기능
-            # track = 0 # 음악 트랙
-            # if re.search(r"\b(다음 노래|다른 음악|)\b", transcript, re.I):
-            #     # 플레이 리스트에 없는 트랙이라면
-            #     if(not track in randomPlayList):
-            #         track = 0 # 트랙 초기화
-            #     # 아니면
-            #     else: 
-            #         track += 1 # 다음 트랙 탐색
-            #     # API.musicPlay(playList, randomPlayList, track)
                 
                 # 대화 배열에 적재하기
                 UUID = str(uuid.uuid1()) # 객체 구별을 위한 랜덤 값 생성
@@ -222,7 +253,6 @@ def main():
             (audio_content=content)
             for content in audio_generator
         )
-        
         responses = client.streaming_recognize(streaming_config, requests)
 
         # 실제로 화면에 뿌려주는 부분
